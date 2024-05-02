925b635fa0651d0b5029c203f7a91871a503d1a5
Katya Philip <philipkatya13@gmail.com>
2024-05-03 00:08:19 +0300

    Lab 5: new socket server

--- CMS/my-backend/socket-server.js
+++ CMS/my-backend/socket-server.js
@@ -1,36 +1,115 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import {formatMessage} from './utils/chatMessage.js';
 
-import { formatMessage } from './utils/chatMessage.js';
-
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const {Server} = require('socket.io');
+const cors = require('cors');
 const chatCollection = 'chats'; // Collection to store all chats
 const userCollection = 'onlineUsers'; // Collection to maintain list of currently online users
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const server = http.createServer(app);
+// const io = new Server(server);
+app.use(cors());
+// MongoDB connection
+// const { MongoClient } = require('mongodb');
+// const mongoUrl = 'mongodb://localhost:27017';
+// const mongoDbName = 'chat_db';
+// const mongoClient = new MongoClient(mongoUrl);
+// async function connectToMongoDB() {
+//     await mongoClient.connect();
+//     console.log("Connected to MongoDB");
+//     return mongoClient.db(mongoDbName);
+// }
+//
+// const mongoDb = connectToMongoDB();
 
-async function main() {
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + 'index.html');
+const {Pool} = require('pg');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const chatSchema = new Schema({
+    owner: String,
+    name: String,
+    members: [String],
+});
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+const messageSchema = new Schema({
+    chatId: String,
+    author: String,
+    order: Number,
+    message: String,
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+mongoose
+    .connect('mongodb://localhost:27017/chat_storage')
+    .then(() => {
+        console.log('Successfully connected');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
     });
 
-    io.on('connection', (socket) => {
-        console.log('a user connected');
+const io = socketIo(server, {
+    cors: {
+        origin: '*',
+    },
+});
 
-        socket.on('chat message', (msg) => {
-            console.log('message: ' + msg);
+// Create a pool to manage database connections
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'embryo',
+    port: 5432,
+});
+app.get('/', (req, res) => {
+    res.send("dummy message");
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('Message received:' + JSON.stringify(msg));
+        io.emit('chat message', msg);
+    });
+
+    socket.on('getMessages', (msg, callback) => {
+        io.emit('getMessageResponse', {"message": "messageResponse"})
+        callback({
+            status: "ok"
         });
-
-        socket.on('disconnect', () => {
-            console.log('user disconnected');
-        });
     });
 
-    server.listen(3000, () => {
-        console.log('listening on *:3000');
+    // socket.on('getChats', (callback) => {
+    //     // query db for chats
+    //     callback({
+    //         status: "ok"
+    //     });
+    // });
+    socket.on('fetch all chats', async (relatedUser) => {
+        try {
+            const existingChats = await Chat.find({
+                $or: [{owner: relatedUser}, {members: {$in: [relatedUser]}}],
+            });
+            socket.emit('all chats', existingChats);
+        } catch (error) {
+            console.error('Error checking chat name availability:', error);
+        }
     });
-}
 
-main();
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
